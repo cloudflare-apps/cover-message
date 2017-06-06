e8badf44b5fe93ab8e728b4da0429d896bc932fb
Teffen Ellis (Eric) <teffen@nirri.us>
2017-06-06 10:29:35 -0400

    Refactor app.

--- source/app.js
+++ source/app.js
@@ -1,146 +1,144 @@
-import {submitConstantContact, submitFormspree, submitMailchimp} from "eager-email-utils"
-import * as renderers from "./renderers"
+import {submitConstantContact, submitFormspree, submitMailchimp} from 'cf-email-utils'
+import * as renderers from './renderers'
 
-(function() {
+(function () {
+  'use strict'
+
   if (!window.addEventListener) return // Check for IE9+
-  const preview = INSTALL_ID === "preview"
+
+  const preview = INSTALL_ID === 'preview'
   let options = INSTALL_OPTIONS
   let element
 
-  function delegateEmailSubmit(receiver, callback) {
-    if (options.signupDestination === "email" && options.email) {
+  function delegateEmailSubmit (receiver, callback) {
+    if (options.signupDestination === 'email' && options.email) {
       submitFormspree(options, receiver, callback)
-    }
-    else if (options.signupDestination === "service") {
-      if (options.account.service === "mailchimp") {
+    } else if (options.signupDestination === 'service') {
+      if (options.account.service === 'mailchimp') {
         submitMailchimp(options, receiver, callback)
-      }
-      else if (options.account.service === "constant-contact") {
+      } else if (options.account.service === 'constant-contact') {
         submitConstantContact(options, receiver, callback)
       }
     }
   }
 
-  function hide(event) {
+  function hide (event) {
     if (event && event.target !== this) return
 
-    element.setAttribute("data-visibility", "hidden")
-    document.body.style.overflow = ""
+    element.setAttribute('data-visibility', 'hidden')
+    document.body.style.overflow = ''
   }
 
   const submitHandlers = {
-    signup(event) {
+    signup (event) {
       event.preventDefault()
 
-      element.setAttribute("data-form", "submitting")
+      element.setAttribute('data-form', 'submitting')
 
       const email = event.target.querySelector("input[name='_replyto']").value
 
       delegateEmailSubmit(email, ok => {
-        element.setAttribute("data-form", "submitted")
-        options.goal = "signupSuccess"
+        element.setAttribute('data-form', 'submitted')
+        options.goal = 'signupSuccess'
 
         if (ok) {
           setTimeout(hide, 3000)
-        }
-        else {
-          options.signupSuccessTitle = "Whoops"
-          options.signupSuccessText = "Something didn’t work. Please check your email address and try again."
+        } else {
+          options.signupSuccessTitle = 'Whoops'
+          options.signupSuccessText = 'Something didn’t work. Please check your email address and try again.'
         }
 
         updateElement()
       })
     },
-    cta(event) {
+    cta (event) {
       event.preventDefault()
 
       if (preview) {
         window.location.reload()
-      }
-      else {
+      } else {
         window.location = options.ctaLinkAddress
       }
     },
-    announcement(event) {
+    announcement (event) {
       event.preventDefault()
 
-      element.setAttribute("data-visibility", "hidden")
+      element.setAttribute('data-visibility', 'hidden')
     }
   }
 
-  function updateElement() {
+  function updateElement () {
     try {
-      localStorage.eagerCoverMessageShown = JSON.stringify(options)
-    }
-    catch (e) {}
+      window.localStorage.cfCoverMessageShown = JSON.stringify(options)
+    } catch (e) {}
 
-    element = Eager.createElement({
-      selector: "body",
-      method: "append"
+    element = INSTALL.createElement({
+      selector: 'body',
+      method: 'append'
     }, element)
 
-    element.classList.add("eager-cover-message")
-    element.setAttribute("data-visibility", "visible")
-    element.setAttribute("data-goal", options.goal)
+    element.setAttribute('app', 'cover-message')
+    element.setAttribute('data-visibility', 'visible')
+    element.setAttribute('data-goal', options.goal)
 
-    document.body.style.overflow = "hidden"
+    document.body.style.overflow = 'hidden'
 
     const children = renderers[options.goal](options)
 
     element.innerHTML = `
-      <eager-backdrop></eager-backdrop>
+      <cf-backdrop></cf-backdrop>
 
-      <eager-dialog>
-        <eager-dialog-content>
-          <eager-dialog-close-button></eager-dialog-close-button>
+      <cf-dialog>
+        <cf-dialog-content>
+          <cf-dialog-close-button role="button"></cf-dialog-close-button>
 
-          <eager-dialog-content-text>
+          <cf-dialog-content-text>
             ${children}
-          </eager-dialog-content-text>
-        </eager-dialog-content>
-      </eager-dialog>
+          </cf-dialog-content-text>
+        </cf-dialog-content>
+      </cf-dialog>
     `
 
-    element.querySelector("eager-dialog").addEventListener("click", hide)
+    element.querySelector('cf-dialog').addEventListener('click', hide)
 
-    const formElement = element.querySelector("form")
-    const closeButton = element.querySelector("eager-dialog-close-button")
+    const formElement = element.querySelector('form')
+    const closeButton = element.querySelector('cf-dialog-close-button')
 
     if (formElement) {
-      formElement.addEventListener("submit", submitHandlers[options.goal])
+      formElement.addEventListener('submit', submitHandlers[options.goal])
       formElement.querySelector("input[type='submit']").style.backgroundColor = options.color
     }
 
-    closeButton.addEventListener("click", hide)
-    element.addEventListener("click", hide)
+    closeButton.addEventListener('click', hide)
+    element.addEventListener('click', hide)
 
-    if (options.goal === "signup" && options.signupDestination === "email" && !options.email) {
+    if (options.goal === 'signup' && options.signupDestination === 'email' && !options.email) {
       const emailInput = element.querySelector("form input[type='email']")
       const submitInput = element.querySelector("form input[type='submit']")
 
-      emailInput.placeholder = "Provide your email in the Eager app installer."
+      element.classList.add('cf-invalid')
+      emailInput.placeholder = 'Provide your email in the Cloudflare app installer.'
       emailInput.disabled = true
       submitInput.disabled = true
     }
   }
 
-  function bootstrap() {
-    const alreadyShown = localStorage.eagerCoverMessageShown === JSON.stringify(options)
+  function bootstrap () {
+    const alreadyShown = window.localStorage.cfCoverMessageShown === JSON.stringify(options)
 
     if (alreadyShown && !preview) return
 
     updateElement()
   }
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", bootstrap)
-  }
-  else {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', bootstrap)
+  } else {
     bootstrap()
   }
 
   window.INSTALL_SCOPE = {
-    setOptions(nextOptions) {
+    setOptions (nextOptions) {
       options = nextOptions
 
       updateElement()
--- source/renderers.js
+++ source/renderers.js
@@ -1,61 +1,61 @@
-import {options} from "../install.json"
+import {options} from '../install.json'
 
 const {properties: schema} = options
-const escapeElement = document.createElement("textarea")
+const escapeElement = document.createElement('textarea')
 
-function esc(content = "") {
+function esc (content = '') {
   escapeElement.textContent = content
 
   return escapeElement.innerHTML
 }
 
-function get(options, key) {
-  return esc(options[key] || schema[key].default || "")
+function get (options, key) {
+  return esc(options[key] || schema[key].default || '')
 }
 
-export function announcement(options) {
+export function announcement (options) {
   return `
-    <eager-dialog-content-title>${get(options, "announcementTitle")}</eager-dialog-content-title>
-    ${get(options, "announcementText")}
+    <cf-dialog-content-title>${get(options, 'announcementTitle')}</cf-dialog-content-title>
+    ${get(options, 'announcementText')}
 
     <form>
-      <input type="submit" value="${get(options, "announcementButtonText")}">
+      <input type="submit" value="${get(options, 'announcementButtonText')}">
     </form>
   `
 }
 
-export function cta(options) {
+export function cta (options) {
   return `
-    <eager-dialog-content-title>${get(options, "ctaTitle")}</eager-dialog-content-title>
+    <cf-dialog-content-title>${get(options, 'ctaTitle')}</cf-dialog-content-title>
 
-    ${get(options, "ctaText")}
+    ${get(options, 'ctaText')}
 
     <form>
-      <input type="submit" value="${get(options, "ctaButtonText")}">
+      <input type="submit" value="${get(options, 'ctaButtonText')}">
     </form>
   `
 }
 
-export function signup(options) {
+export function signup (options) {
   return `
-    <eager-dialog-content-title>${get(options, "signupTitle")}</eager-dialog-content-title>
+    <cf-dialog-content-title>${get(options, 'signupTitle')}</cf-dialog-content-title>
 
-    ${get(options, "signupText")}
+    <cf-signup-text>${get(options, 'signupText')}</cf-signup-text>
 
     <form>
       <input
         name="_replyto"
-        placeholder="${get(options, "signupInputPlaceholder")}"
+        placeholder="${get(options, 'signupInputPlaceholder')}"
         required
         type="email" />
-      <input type="submit" value="${get(options, "signupButtonText")}">
+      <input type="submit" value="${get(options, 'signupButtonText')}">
     </form>
   `
 }
 
-export function signupSuccess(options) {
+export function signupSuccess (options) {
   return `
-    <eager-dialog-content-title>${get(options, "signupSuccessTitle")}</eager-dialog-content-title>
-    ${get(options, "signupSuccessText")}
+    <cf-dialog-content-title>${get(options, 'signupSuccessTitle')}</cf-dialog-content-title>
+    ${get(options, 'signupSuccessText')}
   `
 }
